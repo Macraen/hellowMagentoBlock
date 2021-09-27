0828eeec822232568e5a7335a0bca87fa843aae2
Andrey Puhach <apugach@gomage.com>
2021-09-27 17:22:12 +0300

    1st commit

---
+++ Controller/Index/Display.php
@@ -0,0 +1,24 @@
+<?php
+namespace Magento\Tblock\Controller\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Display extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+//        $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+//        return $page;
+        return $this->_pageFactory->create();
+
+    }
+}
